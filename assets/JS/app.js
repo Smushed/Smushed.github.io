f93df5c9b9a52695fe06d5a67065965739ed0dd4
Smushed <smushedcode@gmail.com>
2018-11-03 19:17:36 -0500

    Updating the portfolio page to start the scrolling

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -2,16 +2,12 @@ let currentSlide = 1;
 
 //TODO Make it so if scrolls to the right or the left
 $(".left-arrow").on("click", function () {
-    // Shows and hides divs based on the current slide
-    $(`.slide${currentSlide}`).hide();
 
-    currentSlide--;
-    if (currentSlide < 1) {
-        //If they are at the beginning of the slides, toggle to the end
-        currentSlide = 2;
-    }
 
-    $(`.slide${currentSlide}`).show();
+    scrollCurrentSlide(currentSlide).then(advancePosition("backwards", currentSlide));
+
+
+    // setTimeout(function () { $(`.slide${currentSlide}`).show() }, 2000);
     console.log(currentSlide)
 })
 
@@ -29,3 +25,26 @@ $(".right-arrow").on("click", function () {
     $(`.slide${currentSlide}`).show();
     console.log(currentSlide)
 })
+
+const scrollCurrentSlide = function (currentSlide) {
+    // Shows and hides divs based on the current slide
+    return new Promise(function () {
+        $(`.slide${currentSlide}`).addClass("fadeOutLeftBig");
+        setTimeout(function () { $(`.slide${currentSlide}`).hide() }, 2000)
+    })
+};
+
+const advancePosition = function (direction, currentSlide) {
+    return new Promise(function () {
+        if (direction === "backwards") {
+            currentSlide--;
+            if (currentSlide < 1) {
+                //If they are at the beginning of the slides, toggle to the end
+                currentSlide = 2;
+            } else if (currentSlide > 2) {
+                currentSlide = 1;
+            }
+        }
+        $(`.slide${currentSlide}`).hide();
+    })
+}
