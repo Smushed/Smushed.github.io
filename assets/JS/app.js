93537afaef0ebc3d0d19e560ab7acaabae3bed72
Smushed <smushedcode@gmail.com>
2018-11-07 21:52:01 -0600

    Have left and right scrolling working, need to focus on getting them to scroll together

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,58 +1,87 @@
 let currentSlide = 0;
 
-//TODO Make it so if scrolls to the right or the left
-$(".left-arrow").on("click", function () {
-    const nextSlide = currentSlide + 1
+$(`.left-arrow`).on(`click`, function () {
+    //Gives which direction the scroll should be going
+    const direction = `backwards`;
 
-    scrollCurrentSlide(currentSlide)
-    advancePosition("backwards")
+    //First this scrolls the current selection out of the way
+    scrollCurrentSlide(direction, currentSlide);
+    //Then it moves the slider one position to the side
+    advancePosition(direction)
+        //Finally it brings in the next slide
+        .then(scrollNextSlide(direction, currentSlide));
 
-
-    // setTimeout(function () { $(`.slide${currentSlide}`).show() }, 2000);
     console.log(currentSlide)
 })
 
-//TODO Make it so if scrolls to the right or the left
 $(`.right-arrow`).on(`click`, function () {
-    // Shows and hides divs based on the current slide
-    $(`.slide${currentSlide}`).hide();
+    //Gives which direction the scroll should be going
+    const direction = `forwards`;
 
-    currentSlide++;
-    if (currentSlide > 2) {
-        //If they are at the beginning of the slides, toggle to the end
-        currentSlide = 1;
-    }
+    //First this scrolls the current selection out of the way
+    scrollCurrentSlide(direction, currentSlide);
+    //Then it moves the slider one position to the side
+    advancePosition(direction)
+        //Finally it brings in the next slide
+        .then(scrollNextSlide(direction, currentSlide));
 
-    $(`.slide${currentSlide}`).show();
     console.log(currentSlide)
 })
 
-const scrollCurrentSlide = function (currentSlide) {
+const scrollCurrentSlide = function (direction, currentSlide) {
     // Shows and hides divs based on the current slide
     return new Promise(function () {
-        $(`.slide${currentSlide}`).addClass(`fadeOutLeftBig`);
-        setTimeout(function () { $(`.slide${currentSlide}`).hide() }, 1900)
+        if (direction === `backwards`) {
+            //Add the class to make the div move
+            $(`.slide${currentSlide}`).addClass(`fadeOutLeftBig onBottom`);
+
+            //setTimeout is to make the div hide just before the animation ends
+            setTimeout(function () {
+                $(`.slide${currentSlide}`).hide().removeClass(`fadeOutLeftBig onBottom`);
+            }, 1900);
+
+        } else if (direction === `forwards`) {
+            //Add the class to make the div move
+            $(`.slide${currentSlide}`).addClass(`fadeOutRightBig onBottom`);
+
+            //setTimeout is to make the div hide just before the animation ends
+            setTimeout(function () {
+                $(`.slide${currentSlide}`).hide().removeClass(`fadeOutRightBig onBottom`);
+            }, 1900);
+        };
     });
 };
 
-const scrollNextSlide = function (nextSlide) {
+const scrollNextSlide = function (direction, currentSlide) {
     //TODO Make the slide come up on button click
     return new Promise(function () {
-        $(`.slide${nextSlide}`).show();
-        $(`.slide${nextSlide}`).addClass(`fadeInRightBig`);
+        if (direction === `backwards`) {
+            $(`.slide${currentSlide}`).show().addClass(`fadeInRightBig onTop`);
+            setTimeout(function () {
+                $(`.slide${currentSlide}`).removeClass(`fadeInRightBig onTop`);
+            }, 2000);
+        } else if (direction === `forwards`) {
+            $(`.slide${currentSlide}`).show().addClass(`fadeInLeftBig onTop`);
+            setTimeout(function () {
+                $(`.slide${currentSlide}`).removeClass(`fadeInLeftBig onTop`);
+            }, 2000);
+        };
     });
 };
 
-const advancePosition = function (direction, currentSlide) {
+const advancePosition = function (direction) {
     return new Promise(function () {
         if (direction === `backwards`) {
             currentSlide--;
             if (currentSlide < 0) {
                 //If they are at the beginning of the slides, toggle to the end
                 currentSlide = 1;
-            } else if (currentSlide > 1) {
+            };
+        } else if (direction === `forwards`) {
+            currentSlide++;
+            if (currentSlide > 1) {
                 currentSlide = 0;
-            }
-        }
-    })
-}
+            };
+        };
+    });
+};
