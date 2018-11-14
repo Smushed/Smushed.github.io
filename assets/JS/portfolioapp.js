0f87b34eb32250c245a2a61aa57af3cd6cea9ed0
Smushed <smushedcode@gmail.com>
2018-11-13 19:29:48 -0600

    Portfolio page now showing the small description on small screens

--- assets/JS/portfolioapp.js
+++ assets/JS/portfolioapp.js
@@ -1,5 +1,5 @@
 // So the carousel can slide left to right with arrow keys
-$(document).keyup(function (e) {
+$(document).keyup(e => {
     const inputKey = e.keyCode;
     // if the input is the left arrow then the previous slide comes up
     if (inputKey === 37) {
