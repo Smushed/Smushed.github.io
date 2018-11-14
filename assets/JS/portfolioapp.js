6d03b5b95c0de899263529e62f7ae4fdc7a1bf46
Smushed <smushedcode@gmail.com>
2018-11-13 18:54:42 -0600

    Updated Portfolio page to add the indicator of what slide you're on. Not sure I like it

---
+++ assets/JS/portfolioapp.js
@@ -0,0 +1,10 @@
+// So the carousel can slide left to right with arrow keys
+$(document).keyup(function (e) {
+    const inputKey = e.keyCode;
+    // if the input is the left arrow then the previous slide comes up
+    if (inputKey === 37) {
+        $(`.carousel`).carousel(`prev`)
+    } else if (inputKey === 39) {
+        $(`.carousel`).carousel(`next`)
+    }
+});
