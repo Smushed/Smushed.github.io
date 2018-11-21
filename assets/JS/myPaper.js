b155cad488d894dce002f01812779cb1eb4194ed
Smushed <smushedcode@gmail.com>
2018-11-20 21:16:06 -0600

    Playing with SVG

--- assets/JS/myPaper.js
+++ assets/JS/myPaper.js
@@ -1,3 +1,9 @@
+//You need live server in order for this to work.
+//The CORS is giving an issue
+
+var newTriangle = new Path.RegularPolygon(new Point(view.center, view.center), 3, 100);
+newTriangle.fillColor = "red"
+
 var path = new Path.Rectangle({
     center: view.center,
     width: 100,
@@ -19,4 +25,4 @@ var myPath = new Path();
 function onResize(event) {
     path.position = view.center;
     myCricle.position = view.center;
-}
+};
